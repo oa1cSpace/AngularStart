06e1329bd7c8dafdd5e2ce9024092fab7bfa0549
oa1cspace <oa1cspace@gmail.com>
2020-09-02 16:50:28 +0300

    raw version of todo-list

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
 
-  constructor() { }
+export class TodoListComponent {
 
-  ngOnInit(): void {
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
   }
 
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteSelectedTodos() {
+    for (let i = (this.todos.length - 1); i > -1; i--) {
+      if (this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
+  }
 }
